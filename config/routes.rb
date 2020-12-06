149271526446c9062b0e117f93ae86379aa8538f
dy.di <d_dy_di@yahoo.co.jp>
2020-12-06 08:06:01 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
   root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :tasks
+  resources :users
 end
